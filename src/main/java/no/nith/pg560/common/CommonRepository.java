49460362cfcfd48130ab32bbe2bc7a406f480b6b
tonnyg <tonny.gundersen@accenture.com>
2011-09-10 21:22:59 +0200

    Changes

---
+++ src/main/java/no/nith/pg560/common/CommonRepository.java
@@ -0,0 +1,57 @@
+package no.nith.pg560.common;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Generisk implementasjon av vanlige dataaksessoperasjoner med JPA.
+ * 
+ * @param <T> Entitetstype i domenemodellen
+ */
+public class CommonRepository<T> {
+    private static final String UGYLDIG_NOEKKELVERDI = "Ugyldig nøkkelverdi";
+    private static final String PARAMETER_IKKE_SATT = "Parameter ikke satt";
+
+    @PersistenceContext(unitName = "pg560")
+    private EntityManager entityManager;
+
+    private final Class<T> domeneType;
+
+    protected CommonRepository(Class<T> domeneType) {
+        this.domeneType = domeneType;
+    }
+
+    protected CommonRepository(Class<T> domeneType, EntityManager em) {
+        this(domeneType);
+        entityManager = em;
+    }
+
+    protected void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public void opprett(T objekt) {
+        if (objekt == null) {
+            throw new IllegalArgumentException(PARAMETER_IKKE_SATT);
+        }
+        entityManager.persist(objekt);
+    }
+
+    public T hent(long id) {
+        if (id < 0) {
+            throw new IllegalArgumentException(UGYLDIG_NOEKKELVERDI);
+        }
+        return entityManager.find(domeneType, id);
+    }
+
+    public T lagre(T objekt) {
+        if (objekt == null) {
+            throw new IllegalArgumentException(PARAMETER_IKKE_SATT);
+        }
+        return entityManager.merge(objekt);
+    }
+}
---
+++ src/main/java/no/nith/pg560/common/Pg560PageNavigation.java
@@ -0,0 +1,11 @@
+package no.nith.pg560.common;
+
+public final class Pg560PageNavigation {
+
+    public static final String LOGIN_PAGE = "login";
+    public static final String MAIN_PAGE = "main";
+
+    private Pg560PageNavigation() {
+
+    }
+}

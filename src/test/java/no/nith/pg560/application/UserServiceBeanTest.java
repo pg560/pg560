ff185f74bdc50ff33c4e37ca572f94b8510fa526
tonnyg <tonny.gundersen@accenture.com>
2011-09-15 00:08:08 +0200

    Changes

--- src/test/java/no/nith/pg560/application/UserServiceBeanTest.java
+++ src/test/java/no/nith/pg560/application/UserServiceBeanTest.java
@@ -2,14 +2,13 @@ package no.nith.pg560.application;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.util.List;
-
 import no.nith.pg560.domain.User;
 import no.nith.pg560.infrastructure.UserJpaRepository;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import static org.mockito.Mockito.when;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -28,8 +27,16 @@ public class UserServiceBeanTest {
 
     @Test
     public void readUsers() throws Exception {
+    	when(userJpaRepository.getUser("Ola", "Ola")).thenReturn(buildUser());
         User user = userServiceBean.getUser("Ola", "Ola");
-        //assertNotNull(user);
+        assertNotNull(user);
     }	
+    
+    private User buildUser() {
+    	User user = new User();
+    	user.setUsername("Ola");
+    	user.setPassword("Ola");
+    	return user;
+    }
 
 }
